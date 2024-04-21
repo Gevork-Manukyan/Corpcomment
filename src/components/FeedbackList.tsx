30d1f2a47fba5e364c500e420f97f05bb5d3e473
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-04-21 16:52:27 -0700

    creates feedbacklist

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,23 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return <ol className="feedback-list">
+    <li className="feedback">
+      <button>
+        <TriangleUpIcon />
+        <span>593</span>
+      </button>
+
+      <div>
+        <p>B</p>
+      </div>
+
+      <div>
+        <p>ByteGrad</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur iure libero tempora aut. Doloribus, tempore.</p>
+      </div>
+
+      <p>4d</p>
+    </li>
+  </ol>;
 }
